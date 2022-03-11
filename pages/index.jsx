2a157ff68bc1a18238af28921126a7cb1fbfdd67
hugojose2505 <hugo.jose.terra@hotmail.com>
2022-03-10 22:01:06 -0300

    add-axios

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,17 @@
 import styles from "../styles/Home.module.scss";
 
-export default function Index() {
+export  function Index() {
   return <h1 className={styles.title}>Hello, Hostel App</h1>;
 }
+
+export const getServerSideProps = async () => {
+    let response;
+      axios.get("https://gestao-hostel-app.herokuapp.com/").then(function (asnwer){
+        console.log(asnwer.data);
+      });
+    return {
+      props:{
+          hello:response
+        }
+    }
+};
---
+++ services/axios.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const instance = axios.create({
+    baseURL: 'https://some-domain.com/api/',
+    timeout: 1000,
+  });
+
